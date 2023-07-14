e6f43b9ea157fa453bb18baf000ff467ca3ad162
Megha Dungriyal <97436039+dungriyalmegha@users.noreply.github.com>
2023-07-14 23:51:05 +0530

    Update admin.py
    Updated the admin.py folder .

--- hack36/blog/admin.py
+++ hack36/blog/admin.py
@@ -1,3 +1,14 @@
+
+
 from django.contrib import admin
 
 # Register your models here.
+# admin.py
+
+from django.contrib.auth.admin import UserAdmin
+from django.contrib.auth.models import User
+
+admin.site.unregister(User)
+admin.site.register(User, UserAdmin)
+
+
